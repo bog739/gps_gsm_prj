7fb8822f403e72a152f487b399d76b5b2752542c
Bogdan <staneseb1@outlook.com>
2023-01-15 20:00:06 +0200

    documentation for project

--- url_format.py
+++ url_format.py
@@ -4,3 +4,6 @@ class URL:
     url_google_maps_satellite = "http://www.google.com/maps/place/#,$/@#,$,19z/data=!3m1!1e3"
     url_google_maps_normal = "http://www.google.com/maps/place/#,/@#,$,19z/data=!3m1!4b1"
     flg_url_state = False
+    
+    
+    
